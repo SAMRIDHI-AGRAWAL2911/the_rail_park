ced75614ab3c6cc34edfc75d9083a1844ebfdd75
SAMRIDHI-AGRAWAL2911 <145381423+SAMRIDHI-AGRAWAL2911@users.noreply.github.com>
2024-01-13 16:42:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
- let clip = document.querySelector(".vid") 
+let clip = document.querySelector(".vid") 
   
 /* Applying mouseover event on video clip  
 and then we call play() function to play  
@@ -6,3 +6,10 @@ the video when the mouse is over the video */
 clip.addEventListener("mouseover", function (e) { 
     clip.play(); 
 })
+const hamburger=document.querySelector(".hamburger");
+const navMenu=document.querySelector(".nav-menu");
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+document.querySelectorAll(".nav-link").forEach(n=>n.add)
